d7c3ac929a512b817a42970819612bf196adfcfa
Claire LESAFFRE <clesaffr@e2r10p4.42.fr>
2021-02-02 07:47:03 +0100

    cleaned

--- ft_int.c
+++ ft_int.c
@@ -6,11 +6,26 @@
 /*   By: clesaffr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 14:50:28 by clesaffr          #+#    #+#             */
-/*   Updated: 2021/01/27 17:23:08 by clesaffr         ###   ########.fr       */
+/*   Updated: 2021/01/28 16:25:38 by clesaffr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+static void	ft_dot(t_indic *flag, char *str)
+{
+		if (flag->negative)
+		{
+			ft_putchar('-');
+			print_width(flag->dot, ft_strlen(str) - 1, 1);
+			ft_putstr(str + 1);
+		}
+		else
+		{
+			print_width(flag->dot, ft_strlen(str), 1);
+			ft_putstr(str);
+		}
+}
 
 int	ft_int(t_indic *flag, va_list *va)
 {
@@ -19,12 +34,16 @@ int	ft_int(t_indic *flag, va_list *va)
 
 	nbr = va_arg(*va, int);
 	str = ft_itoa(nbr);
+	if (nbr < 0)
+		flag->negative = 1;
+	else
+		flag->negative = 0;
 	if (flag->zero)
 	{
-		if (nbr < 0)
+		if (flag->negative)
 		{
 			ft_putchar('-');
-			print_width((flag->width - 1), ft_strlen(str), 1);
+			print_width((flag->width), ft_strlen(str), 1);
 			ft_putstr(str + 1);
 		}
 		else
@@ -36,23 +55,18 @@ int	ft_int(t_indic *flag, va_list *va)
 	}
 	if (flag->minus)
 	{
-		ft_putstr(str);
-		print_width(flag->width, ft_strlen(str), 0);
+		if (flag->dot > 0)
+			ft_dot(flag, str);
+		else
+			ft_putstr(str);
+		print_width(flag->width, ft_strlen(str), 0); //RAJOUT D'UN MOINS 2 POUR PETITE STR
 		return (0);
 	}
-	if (flag->dot)
+	if (flag->dot > 0)
 	{
-		if (nbr < 0)
-		{
-			ft_putchar('-');
-			print_width(flag->dot, ft_strlen(str), 1);
-			ft_putstr(str + 1);
-		}
-		else
-		{
-			print_width(flag->dot, ft_strlen(str), 1);
-			ft_putstr(str);
-		}
+		if (flag->width)
+			print_width(flag->width, ft_strlen(str) + flag->negative, 0); //-1 BIG STR // +1 SMOL STR
+		ft_dot(flag, str);
 		return (0);
 	}
 	print_width(flag->width, ft_strlen(str), 0);
